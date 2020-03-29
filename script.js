6efc95b12a8aa32d8a5f1e24c09ca41ccec8ff63
Apa6 <alim.gazdieff@gmail.com>
2020-03-29 20:39:06 +0500

    methods in primitives

---
+++ script.js
@@ -0,0 +1,26 @@
+//! МЕТОДЫ У ПРИМИТИВОВ 
+
+//? Примитив как объект (работа со строками)
+
+  // let str = "Hello";          //  у примитивов (str,nubmer,boolean..) имеются определенные методы( которые позволяют работать с ними)
+  // console.log(str.toUpperCase());   // записывает все буквы в строке в верхнем регистре
+
+//? Примитив как объект (работа с числами)
+
+  // let num = 1.234245;
+  // console.log(num.toFixed(1))       // округляет число до одного значения
+
+//? Примитив как объект (строку,boolean в число и обратно)
+
+  // let boolean = true;
+  // console.log(Number(boolean));
+
+  // let str = "123";
+  // console.log(Number(str));
+
+  // let num = 123;
+  // console.log(String(num))
+
+//? Примитив как объект (null, undefined) не имеют методов
+
+  // console.log(null.test())  // error
